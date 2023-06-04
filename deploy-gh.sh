cbdb0b0a5dc236ce767ce9704f38c497af6684b5
jacksontao <lit9771@gmail.com>
2023-06-04 15:26:38 +0800

    index.html

---
+++ deploy-gh.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 把上面的 <USERNAME> 换成你自己的 Github 用户名，<REPO> 换成仓库名，比如我这里就是：
+git push -f git@github.com:jacksontao/ConvertWeb.git master:gh-pages
+
+cd -
---
+++ docs/vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    title: "My Blog",
+    description: "This is a blog.",
+    base: '/ConvertWeb/',
+    themeConfig:{
+        nav:[
+            { text: '主页', link: '/' },
+            { text: '软件', link: '/Software/' },
+            { text: '教程', link: '/Tutorials/' },
+            { text: '学习', link: '/Study/' },
+            { text: '联系', link: '/Contact/' },
+            { text: 'CSDN', link: 'https://blog.csdn.net/qq_33704787' }
+        ]
+    }
+
+}
